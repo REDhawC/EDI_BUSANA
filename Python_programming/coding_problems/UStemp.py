0e8179bd4254b5f08a57bd8980615587c6eb4b6a
REDhawC <redhawc1@gmail.com>
2023-11-25 11:10:43 +0000

    py_ASM3th

---
+++ Python_programming/coding_problems/UStemp.py
@@ -0,0 +1,2 @@
+# "https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/statewide/time-series/1/tavg/all/10/2021-2023.csv?base_prd=true&begbaseyear=1901&endbaseyear=2000"
+# "https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/statewide/time-series/1/pcp/all/10/2021-2023.csv?base_prd=true&begbaseyear=1901&endbaseyear=2000"
